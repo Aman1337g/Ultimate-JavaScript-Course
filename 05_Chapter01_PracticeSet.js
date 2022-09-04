1d3759f6bc90c0a56508699fced5c729f299db24
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-04 21:54:04 +0530

    05_Chapter01_PracticeSet

---
+++ 05_Chapter01_PracticeSet.js
@@ -0,0 +1,34 @@
+//01
+let a = "aman" + 1;
+console.log(a)
+let c = 389
+console.log(a+c)
+
+//02
+console.log(typeof a)
+
+//03
+const b =  {
+  "aman": "good boy",
+  "shobhan": 47, 
+  roshan: 39
+}
+console.log(b["shobhan"])
+console.log(b["roshan"])
+console.log(b.shobhan)
+console.log(b)
+
+// const b = 39;
+
+//04
+b['rohan']= "gangster"
+console.log(b)
+
+//05
+const dict = {
+  laureate: "deserving or having special recognition for achievement, as for poetry",
+  experience: "a particular instance of personally encountering or undergoing something"
+}
+console.log(dict)
+console.log(dict.laureate)
+console.log(dict['laureate'])
