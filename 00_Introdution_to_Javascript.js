d165adae8aaa8b0f8e9b0394d1a849f832f3f64e
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-04 12:25:16 +0530

    Added 00_Introdution_to_Javascript

---
+++ 00_Introdution_to_Javascript.js
@@ -0,0 +1,8 @@
+console.log("Hello World")    // console.log for priting something
+
+function addThree(a,b,c) {    // Created a function addThree taking three arguements and adding them
+  return a+b+c;
+}
+
+let c = addThree(12,33,2)     // variable 'C' storing the value returned by the function addThree and storing it 
+console.log(c)
