d7235414ec27c45c30507d93434cd4d1bb1fcf90
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-04 21:47:30 +0530

    Added 01_Variables_in_Javascript

---
+++ 01_Variables_in_Javascript.js
@@ -0,0 +1,9 @@
+// let has block level scope
+// var has global level scope
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+var a = 67 // a contains 67
+console.log(a)
+a = "Aman"    // 'a' storing a string value
+console.log(a)
+// let 8aman = 7 // Not allowed this will throw an error
+// let var = 7
