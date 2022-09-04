7a7acc1ca5d6f1a9eba624fc45baff0fd12d8b11
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-04 22:11:27 +0530

    Added 06_Operators

---
+++ 06_Operators.js
@@ -0,0 +1,46 @@
+// "aman";
+// 48;
+// true;
+console.log("Operators in JS")
+let a=45;
+let b=4;
+//Arithmetic Operators
+console.log("a+b =",a+b)
+console.log("a-b =",a-b)
+console.log("a/b =",a/b)
+console.log("a*b =",a*b)
+console.log("a**b =",a**b)
+console.log("a%b =",a%b)
+console.log("a++ =",a++)
+console.log("a =",a)
+console.log("a-- =",a--)
+console.log("a =",a)
+
+//Assignment Operators
+a = 3
+console.log("a += b =",a+=b)
+console.log("a -= b =",a-=b)
+console.log("a /= b =",a/=b)
+console.log("a %= b =",a%=b)
+console.log("a *= b =",a*=b)
+console.log("a **= b =",a**=b)
+
+//Comparison Operators
+let comp1=8;
+let comp2="8";
+console.log("comp1 == comp2 is",comp1==comp2)
+console.log("comp1 != comp2 is",comp1!=comp2)
+console.log("comp1 === comp2 is",comp1===comp2)
+console.log("comp1 !== comp2 is",comp1!==comp2)
+console.log("comp1 > comp2 is",comp1>comp2)
+console.log("comp1 < comp2 is",comp1<comp2)
+console.log("comp1 >= comp2 is",comp1>=comp2)
+console.log("comp1 <= comp2 is",comp1<=comp2)
+
+//Logical Operators
+let logi1=3
+let logi2=4
+console.log("logi1 > logi2 && logi1 == 3 is",logi1 > logi2 && logi1==3)
+console.log("logi1 > logi2 || logi1 == 3 is",logi1 > logi2 || logi1==3)
+console.log(!true)
+console.log(!false)
