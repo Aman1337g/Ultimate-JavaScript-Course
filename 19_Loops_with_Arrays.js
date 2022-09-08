1c239a116a8d972bb4092319c184cd958a0ca808
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-09 01:08:57 +0530

    Added 19_Loops_with_Arrays

---
+++ 19_Loops_with_Arrays.js
@@ -0,0 +1,48 @@
+let arr = [2, 54, 7, 6]
+
+// printing elements in array
+// using classical for loop
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+}
+console.log("\n")
+
+// using forEach() loop
+// As it is clear from its name , applies to each element in array
+arr.forEach((element) => {
+  console.log(element * element)
+})
+// another way
+console.log("\n")
+let square = (element) => {
+  console.log(element * element)
+  // return element * element
+}
+arr.forEach(square)
+// let num = []
+// console.log(num)
+
+// let r = num.splice(arr.forEach(square))
+// console.log(r)
+console.log("\n")
+
+// using Array.from()
+// used to create an array from any other object
+// eg. - used to create an array from 'html collection' [will see when working will DOM] = for more information , go to - 
+// "https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp"
+// if we have html collection we cannot use forEach() we have to use Array.from() to convert it to array first
+let name = "Aman Kumar Gupta"
+console.log(name)
+let ar = Array.from(name)
+console.log(ar)
+
+// for...of loop
+for (let i of arr) {
+  console.log(i)
+}
+console.log("\n")
+
+// for...in loop
+for (let i in arr) {
+  console.log(`arr[${i}] : `, arr[i])
+}
