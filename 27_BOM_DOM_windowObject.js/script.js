81398dcba524f97ce012e0b3f4bc8b4ba9a62868
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-19 01:47:03 +0530

    Added 27_BOM_DOM_windowObject.js

---
+++ 27_BOM_DOM_windowObject.js/script.js
@@ -0,0 +1,8 @@
+console.log(window)
+// OR
+window.console.log(window)
+
+console.log(document)
+console.log(document.body)
+document.body.style.background = "#ff3131"
+// location.href = "https://www.brave.com"  
