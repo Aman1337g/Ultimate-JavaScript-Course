fe7cc0c2132f88e98eed6e368a2ca7fedf5553a8
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-25 19:27:04 +0530

    Added 30_Walking_the_DOM.js

---
+++ 30_Walking_the_DOM.js/script.js
@@ -0,0 +1,8 @@
+document.title = 'code , coffee and coders'
+// title can only be changed at the user's side not on the server's side
+console.log(document.title)
+console.log(typeof document.title)
+
+console.log(document.head)
+console.log(document.body)
+// documet.title returns string while document.body or document.head returns tag itself
