64c34fbb9279510ac346e81e369a5f764c475aea
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-10-22 21:57:48 +0530

    Added 32_Accessing_Children_of_an_Element.js

---
+++ 32_Accessing_Children_of_an_Element.js/script.js
@@ -0,0 +1,9 @@
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[document.body.childNodes.length - 1])
+
+
+let arr = Array.from(document.body.childNodes)
+console.log(arr)
+console.log(document.body.haschildNodes)
